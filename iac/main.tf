bc40603532ad1e6655798f1479d579fb13a4d7cb
patrickm663 <rev0cation2025@outlook.com>
2023-06-29 09:50:34 +0200

    Feat: add S3 IaC

---
+++ iac/main.tf
@@ -0,0 +1,12 @@
+# Configure Terraform
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 4.0"
+        }
+    }
+}
+
+# Configure AWS Provider
+provider "aws" {}
---
+++ iac/s3.tf
@@ -0,0 +1,12 @@
+// S3 bucket for CV
+resource "aws_s3_bucket" "bucket" {
+    bucket = "patrick-moehrke-cv"
+}
+
+// Allow public access
+resource "aws_s3_bucket_public_access_block" "bucket" {
+    bucket = aws_s3_bucket.bucket.id
+
+    block_public_acls 	= false
+    block_public_policy	= false
+}

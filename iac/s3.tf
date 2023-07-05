1e064b3ebdfee129ee3fd435cc984786da7d617d
patrickm663 <rev0cation2025@outlook.com>
2023-07-05 19:24:10 +0200

    IaC: add public access ACL rule

--- iac/s3.tf
+++ iac/s3.tf
@@ -10,3 +10,8 @@ resource "aws_s3_bucket_public_access_block" "bucket" {
     block_public_acls 	= false
     block_public_policy	= false
 }
+
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  bucket = aws_s3_bucket.My_bucket.id
+  acl    = "public-read"
+}

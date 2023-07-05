1a3550d07440fa6f290a89ee4d679092cd31a617
patrickm663 <rev0cation2025@outlook.com>
2023-07-05 20:03:18 +0200

    IaC: add policy for public access

--- iac/s3.tf
+++ iac/s3.tf
@@ -1,19 +1,37 @@
 // S3 bucket for CV
 resource "aws_s3_bucket" "bucket" {
     bucket = "patrick-moehrke-cv-pdf"
-    acl = ""
 }
 
 // Allow public access
 resource "aws_s3_bucket_public_access_block" "bucket" {
     bucket = aws_s3_bucket.bucket.id
-
-    block_public_acls 	= false
-    block_public_policy	= false
+    block_public_acls 		= false
+    block_public_policy		= false
+    ignore_public_acls 		= false
+    restrict_public_buckets 	= false
 }
 
-// Allpw public access
-resource "aws_s3_bucket_acl" "bucket_acl" {
-  bucket = aws_s3_bucket.bucket.id
-  acl    = "public-read"
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.bucket.bucket
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression's result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Id      = "CVBUCKETPOLICY"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = [
+                "s3:GetObject"
+            ]
+        Resource = [
+          "${aws_s3_bucket.bucket.arn}/*",
+        ]
+      },
+    ]
+  })
 }
